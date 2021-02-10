6483802accab4a7f45e75ad67f96f7e168688091
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-02-10 21:51:35 +0700

    ADD:JSON Web Token(JWT)and Middleware Authentication

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ const { Schema } = mongoose;
 // Using cors `require()
 const cors = require("cors");
 
+// Using jwt `require()  (importing jwt)
+const jwt = require("jsonwebtoken");
+
 require("dotenv").config();
 
 // connect  mongoose to MongoDB.
@@ -46,11 +49,36 @@ app.use(cors());
 // Parse JSON bodies for this app.
 app.use(express.json());
 
+// (Middleware) function to authenticate token
+function authenticateToken(req, res, next) {
+  console.log(req.headers);
+  const authHeaderToken = req.headers["authorization"];
+  // if there is no token
+  if (!authHeaderToken) return res.sendStatus(401);
+  // if there is token then  verify
+  jwt.verify(authHeaderToken, "asdl4u47jj4dj", (err, user) => {
+    // token is not vaild
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
+
 // using the get method
 // LOGIC for the Get Request
 // here we are trying to get the data
 app.get("/", (req, res) => {
   res.send("Creating My First Node JS API");
+});
+
+// using the get method
+// LOGIC for the Get Request
+// Middleware
+app.get("/wishlist", authenticateToken, (req, res) => {
+  console.log("Am Authenticated ", req.user);
+  res.send({
+    items: ["The Avengers", "Tenet", "Queens Gambit"],
+  });
 });
 
 app.post("/register", (req, res) => {
@@ -61,16 +89,20 @@ app.post("/register", (req, res) => {
   });
 
   // Save Model to Database
+  // save user data to dataBase
   newUser.save((err, user) => {
+    //if user already exist(already been register)
     if (err) {
       console.log(err);
       res.send(400, {
         status: err,
       });
     } else {
-      console.log("all is good");
-      console.log(user);
-      res.send("registered");
+      // console.log("all is good");
+      // console.log(user);
+      res.send({
+        status: "registered",
+      });
     }
   });
 });
@@ -83,11 +115,14 @@ app.post("/login", (req, res) => {
   const email = req.body.email;
 
   User.findOne({ email: email, password: password }, (err, user) => {
-    console.log(user);
+    // console.log(user);
+
+    const token = genereateAccessToken(user);
+    console.log("the web token for this user  is : ", token);
     if (user) {
       res.send({
         status: "Valid",
-        token: user.id, // set the token as the user ID
+        token: token, // return token back to the fronEnd
       });
     } else {
       res.send(404, {
@@ -102,3 +137,13 @@ app.post("/login", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
+
+// function Generating a Token
+function genereateAccessToken(user) {
+  const payload = {
+    id: user.id,
+    name: user.name,
+  };
+  // expires after  2 days (172800 seconds)
+  return jwt.sign(payload, "asdl4u47jj4dj", { expiresIn: "172800s" });
+}
