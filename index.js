5a252d3e90fbc4a6e4f1fee452e6361fa68e96a5
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-02-22 21:35:37 +0700

    ADD: clean code and add comment

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// importing express framework from node_modules
+// import express framework from node_modules
 const express = require("express");
 
 // initializ express
@@ -7,21 +7,29 @@ const app = express();
 // setting the port
 const port = 3000;
 
-// Using Node.js `require()  (importing mongoose from node_modules)
-const mongoose = require("mongoose");
-
-// Grab The Schema Object from  mongoose
-const { Schema } = mongoose;
-
-// Using cors  (importing cors from node_modules)
+// import cors from node_modules (Using cors)
 const cors = require("cors");
 
-// Using jwt `require() (importing jwt from node_modules)
+// import jwt from node_modules (Using jwt)
 const jwt = require("jsonwebtoken");
 
+// require dotenv(to manage secrets and configs)
+// Using dotenv package to create environment variables
 require("dotenv").config();
 
-// connect  mongoose to MongoDB.
+// determine which domain can access the website
+app.use(cors());
+
+// Parse JSON bodies for this app.
+app.use(express.json());
+
+// import mongoose from node_modules
+const mongoose = require("mongoose");
+
+// Grab The Schema Object from mongoose
+const { Schema } = mongoose;
+
+// connect mongoose to MongoDB.
 mongoose
   .connect(
     `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.ffror.mongodb.net/netflix-api-db-dev?retryWrites=true&w=majority`,
@@ -34,7 +42,8 @@ mongoose
   .then(() => console.log("conneted to DB ..."))
   .catch((error) => console.log(error));
 
-// define  user a schema
+// define user a schema
+
 const User = mongoose.model(
   "Users",
   new Schema({
@@ -52,7 +61,9 @@ const User = mongoose.model(
 );
 
 // define Wishlist a schema
+
 const WishListSchema = new Schema({
+  // user Id is reference to user ID in DB (Foreign-key mongoose)
   user: {
     type: Schema.Types.ObjectId,
     ref: "Users",
@@ -67,32 +78,106 @@ WishListSchema.index({ user: 1, movieId: 1 }, { unique: true });
 
 const WishList = mongoose.model("WishList", WishListSchema);
 
-// determine which domain can access the website
-app.use(cors());
+/**
+ * function for Generating Token
+ */
 
-// Parse JSON bodies for this app.
-app.use(express.json());
+const genereateAccessToken = (user) => {
+  const payload = {
+    id: user.id,
+    name: user.name,
+  };
+  // expires after 2 days (172800 seconds)
+  //  "asdl4u47jj4dj" is the Secret Key
+  return jwt.sign(payload, "asdl4u47jj4dj", { expiresIn: "7200s" });
+};
 
-// (Middleware) function to authenticate token
-function authenticateToken(req, res, next) {
-  console.log(req.headers);
+/**
+ *  Middleware function to authenticate token
+ */
+
+const authenticateToken = (req, res, next) => {
+  // console.log("req.headers is ", req.headers);
+  // get jwt Token from the request header
   const authHeaderToken = req.headers["authorization"];
+
   // if there is no token
+  // HTTP Status 401 mean Unauthorized
   if (!authHeaderToken) return res.sendStatus(401);
-  // if there is token then  verify
+
+  // if there is token then verify
   jwt.verify(authHeaderToken, "asdl4u47jj4dj", (err, user) => {
-    // token is not vaild
+    // token is not vaild (token is expired)
+    // HTTP Status 401 mean Forbidden
     if (err) return res.sendStatus(403);
+
+    // console.log("The Authorized User is ", user);
     req.user = user;
     next();
   });
-}
+};
 
-// using the get method
-// LOGIC for the Get Request
-// here we are trying to get the data
+/**
+ * using the get method
+ * LOGIC for the Get Request
+ * here we are trying to get the data
+ */
+
 app.get("/", (req, res) => {
   res.send("Creating My First Node JS API");
+});
+
+app.post("/register", (req, res) => {
+  const newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  // Save Model to Database(save user data to dataBase)
+  newUser.save((err, user) => {
+    if (err) {
+      //if user already exist(already been register)
+      res.send(400, {
+        status: err,
+      });
+    } else {
+      // console.log("all is good");
+      // console.log(user);
+      res.send({
+        status: "registered",
+      });
+    }
+  });
+});
+
+/**
+ * find the user with the given email and password in the database
+ * if user is valid(been fund in DB) Generate AccessToken to the user
+ * and return the AccessToken to the frondEnd
+ * but if user is not found then return  404 status: "Not Found"
+ */
+
+app.post("/login", (req, res) => {
+  // JavaScript object containing the parse JSON
+  // console.log(req.body);
+  const password = req.body.password;
+  const email = req.body.email;
+  User.findOne({ email: email, password: password }, (err, user) => {
+    // console.log(user);
+    if (user) {
+      const token = genereateAccessToken(user);
+      console.log("The AccessToken is : ", token);
+      res.send({
+        status: "valid",
+        token: token, // return token back to the fronEnd
+      });
+    } else {
+      res.send(404, {
+        status: "Not Found",
+      });
+    }
+  });
 });
 
 app.post("/wishlist", authenticateToken, (req, res) => {
@@ -112,17 +197,21 @@ app.post("/wishlist", authenticateToken, (req, res) => {
       });
     } else {
       res.send({
-        wishlistItem: wishlistItem,
+        wishlistItem: wishlistItem, // return the wishlistItem
         status: "saved",
       });
     }
   });
 });
 
-// using the get method
-// LOGIC for the Get Request
-// Middleware
+/**
+ * using the get method
+ * LOGIC for the Get Request
+ * Middleware
+ */
+
 app.get("/wishlist", authenticateToken, (req, res) => {
+  console.log("I am  authenticated User ", req.user);
   WishList.find({ user: req.user.id }, (err, docs) => {
     if (err) {
       res.send(400, {
@@ -131,71 +220,17 @@ app.get("/wishlist", authenticateToken, (req, res) => {
     } else {
       res.send({
         status: "good",
-        results: docs,
+        results: docs, // return wishlist movies
       });
     }
   });
 });
 
-app.post("/register", (req, res) => {
-  const newUser = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-  });
+/**
+ * start our app
+ * listening to the port
+ */
 
-  // Save Model to Database
-  // save user data to dataBase
-  newUser.save((err, user) => {
-    if (err) {
-      //if user already exist(already been register)
-      res.send(400, {
-        status: err,
-      });
-    } else {
-      // console.log("all is good");
-      // console.log(user);
-      res.send({
-        status: "registered",
-      });
-    }
-  });
-});
-
-// function Generating a Token
-function genereateAccessToken(user) {
-  const payload = {
-    id: user.id,
-    name: user.name,
-  };
-  // expires after  2 days (172800 seconds)
-  return jwt.sign(payload, "asdl4u47jj4dj", { expiresIn: "7200s" });
-}
-
-app.post("/login", (req, res) => {
-  // JavaScript object containing the parse JSON
-  // console.log(req.body);
-
-  const password = req.body.password;
-  const email = req.body.email;
-  User.findOne({ email: email, password: password }, (err, user) => {
-    // console.log(user);
-    if (user) {
-      const token = genereateAccessToken(user);
-      res.send({
-        status: "valid",
-        token: token,
-      });
-    } else {
-      res.send(404, {
-        status: "Not Found", // return token back to the fronEnd
-      });
-    }
-  });
-});
-
-// start our app
-// listening to the port
 app.listen(process.env.PORT || port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost: ${port}`);
 });
