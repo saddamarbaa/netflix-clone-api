c776942c0b1dd9e08fc74cf4ff5ae40de242bbcf
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-04-30 10:28:24 +0700

    ADD: Heroku changes

--- controllers/users.js
+++ controllers/users.js
@@ -2,7 +2,7 @@
 
 const bcrypt = require("bcryptjs");
 const generateAccessToken = require("../auths/getToken");
-const User = require("../models/User");
+const User = require("../models/user");
 const mongoose = require("mongoose");
 
 // API Endpoint for Handling Post Request to /User/login
